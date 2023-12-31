4a2a90a4f427b3f2a9f8a35f12e272f07bc95a45
Mücahit Tezcan <mtezcan@MagiClick.com>
2023-12-31 01:09:27 +0000

    packages globally

--- inputOutput/main.go
+++ inputOutput/main.go
@@ -1,8 +1,8 @@
 package main
 
 func main() {
-    var message string = "hello form go"
-    var price = 34.5
+	var message string = "hello form go"
+	printData()
 
-    print(message, price)
+	print(message)
 }
