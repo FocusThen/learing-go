6dc90b7acf3d418fb67fa48065c0c4447ebe203e
Mücahit Tezcan <mtezcan@MagiClick.com>
2024-01-01 21:54:42 +0000

    function and multiple return value

--- inputOutput/main.go
+++ inputOutput/main.go
@@ -1,7 +1,16 @@
 package main
 import ("example.com/go/io/data"; "fmt")
 
+
+func calculateTax(price float32) (float32, float32) {
+    return price * 0.08, price * 0.09
+}
+
 func main() {
+    // how to use functions and multiple return values
+    stateTax, federalTax := calculateTax(100)
+
+    fmt.Println(stateTax, federalTax)
     fmt.Println(data.MinSpeed)
 	printData()
 }
