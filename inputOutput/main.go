90fe7c09651fefd62852fd18c3402bd8b593bd1c
Mücahit Tezcan <mtezcan@MagiClick.com>
2024-01-01 22:01:11 +0000

    local names and returns

--- inputOutput/main.go
+++ inputOutput/main.go
@@ -6,11 +6,17 @@ func calculateTax(price float32) (float32, float32) {
     return price * 0.08, price * 0.09
 }
 
+func calculateTaxWithName (price float32) (stateTax float32, fedaralTax float32) {
+    stateTax = price * 0.08
+    fedaralTax = price * 0.09
+    return
+}
+
 func main() {
     // how to use functions and multiple return values
-    stateTax, federalTax := calculateTax(100)
+    _, federalTax := calculateTaxWithName(100)
 
-    fmt.Println(stateTax, federalTax)
+    fmt.Println(federalTax)
     fmt.Println(data.MinSpeed)
 	printData()
 }
